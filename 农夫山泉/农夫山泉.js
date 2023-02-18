3e99fbf9132069665e4571938c5b7fd38afeb04b
duoluodexiaoxiaoyuan <1203125214@qq.com>
2023-02-18 10:47:54 +0800

    使用chatGPT咨询的问题

---
+++ 农夫山泉/农夫山泉.js
@@ -0,0 +1,7 @@
+launchApp('送水到府')
+setTimeout(() => {
+  console.show()
+  console.info(id("rv_list").findOne())
+  console.info(text("大包装水").findOne())
+  // id("rv_list").findOne().click()
+}, 2000);
